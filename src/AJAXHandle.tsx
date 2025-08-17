1ec43b2c690cb281830c7ceef81561754c6a7a4b
Malindu Abeysekara <malindueran.cwork@gmail.com>
2025-08-17 15:13:36 +0530

    AJAX prac

--- src/AJAXHandle.tsx
+++ src/AJAXHandle.tsx
@@ -2,7 +2,25 @@ export const AJAXHandle = () =>{
     const getData = () =>{
         //handle AJAX
         const xhr = new XMLHttpRequest();
+         xhr.open("GET","https://jsonplaceholder.typicode.com/posts",true);
+
+        xhr.onload = ()=>{
+          if(xhr.status === 200){
+              console.log(xhr.responseText)
+          }
+        };
+        xhr.onerror = () =>{
+          console.log("Request Failed")
+        };
+        xhr.send();
+
+        
     }
+        const getDataFetch = () =>{
+            fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(data =>{data.json()})
+            .catch(err=>console.error(err))
+        }
     
     
     return(
@@ -10,6 +28,7 @@ export const AJAXHandle = () =>{
         <h1>this is AJAX handle page</h1>
         <br />
         <button onClick={getData}>Get data</button>
+        <button onClick={getDataFetch}>Get data</button>
         </>
     )
 }
