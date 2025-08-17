afac1b8cbd5dbc2d80a03ee1599a173c485011ec
Malindu Abeysekara <malindueran.cwork@gmail.com>
2025-08-17 14:35:31 +0530

    WIP: my changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import './App.css';
 import { Test } from './Test';
 import { Concept } from './Concept';
 import { CourseMaterial } from './components/CourseMaterial';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
    <>
+    <NavBar/>
     <CourseMaterial/>
    </>
   );
--- src/components/CourseMaterial.tsx
+++ src/components/CourseMaterial.tsx
@@ -1,7 +1,37 @@
+import Table from 'react-bootstrap/Table';
+
 export const CourseMaterial = () =>{
+    const tblHeaders =[
+      "Material Id",
+      "File Name",
+      "Material Name",
+      "Material ",
+      "Upload Time",
+      "Course Id",
+      "Option"
+    ]
     return(
         <>
-        <h1>cm</h1>
-        </>
-    )
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            {tblHeaders.map((headings,index)=>(
+                   <th key={index}>{headings}</th>
+            ))};
+         </tr>
+        </thead>
+        <tbody>
+          <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </Table>
+    </>
+  );
 }
